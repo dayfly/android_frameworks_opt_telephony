d8b832a98a81bc19d609e6fa92f652d4500b4a9f
dayfly <neve100@nate.com>
2014-12-13 12:59:49 +0900

    Revert "d1lu: Fix LGT SMS issue"
    This reverts commit ec8d9639d280ec25765cffc14930283ad44e5b29.

--- src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
+++ src/java/com/android/internal/telephony/cdma/CdmaServiceStateTracker.java
@@ -405,8 +405,6 @@ public class CdmaServiceStateTracker extends ServiceStateTracker {
                     mMin = cdmaSubscription[3];
                     mPrlVersion = cdmaSubscription[4];
                     if (DBG) log("GET_CDMA_SUBSCRIPTION: MDN=" + mMdn);
-//cm10 f160l
-		    com.android.internal.telephony.cdma.sms.BearerData.callbackAddr = mMdn;
 
                     mIsMinInfoReady = true;
 
